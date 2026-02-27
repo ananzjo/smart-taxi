86fa7cb5ea6dd274c9ba4b3823315e3cb210f973
Anan Zitawi <35911437+ananzjo@users.noreply.github.com>
2026-02-27 04:34:43 +0300

    Delete V2 directory

--- V2/global-config.js
+++
@@ -1,6 +0,0 @@
-// إعدادات الربط العامة مع سوبابيس
-const SB_URL = "https://jmalxvhumgygqxqislut.supabase.co".trim();
-const SB_KEY = "sb_publishable_62y7dPN9SEc4U_8Lpu4ZNQ_H1PLDVv8".trim();
-const _supabase = supabase.createClient(SB_URL, SB_KEY);
-
-// أي إعدادات عامة أخرى تضاف هنا مستقبلاً
